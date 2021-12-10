30f0a428d4ee3f41bc6bb7c57a02228f9bd9ad52
HeenriqueCDS <88007439+HeenriqueCDS@users.noreply.github.com>
2021-12-10 15:49:05 -0300

    Stripe Payments

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,13 +1,41 @@
+import { signIn, useSession } from 'next-auth/react'
+
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss'
 
 type SubscribeButtonProps = {
     price: string;
 }
 
-export function SubscribeButton({price} : SubscribeButtonProps) {
+export function SubscribeButton({ price }: SubscribeButtonProps) {
+    const session = useSession();
+    async function handleSubscribe() {
+        if (!session.data) {
+            signIn('github')
+            return;
+        }
+
+        try {
+            const stripe = await getStripeJs()
+
+            const response = await api.post('/subscribe')
+            const { sessionId } = response.data
+            await stripe.redirectToCheckout({
+                sessionId,
+            })
+
+        } catch(err) {
+            alert(err.message)
+            return;
+        }
+
+    }
     return (
         <button
             type='button'
+            onClick={handleSubscribe}
             className={styles.subscribeButton}
         >
             Subscribe now
